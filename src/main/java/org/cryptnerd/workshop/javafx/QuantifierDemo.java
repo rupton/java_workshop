0f5b16d11dae6f4ddcaf3b08a08dc70d5cb43487
rupton <ryanjupton@gmail.com>
2026-05-15 12:19:05 -0400

    Added quantifier class demo

---
+++ src/main/java/org/cryptnerd/workshop/javafx/QuantifierDemo.java
@@ -0,0 +1,19 @@
+package org.cryptnerd.workshop.javafx;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class QuantifierDemo {
+
+	public static void main(String[] args) {
+		String[] strings = {"CCQQ", "QQCC", "CQQ", "QUACK", "CCQ", "CQC", "CQQQ", "CCCQQ"};
+
+	    for(String s: strings){
+	      Pattern pattern = Pattern.compile("C+Q{2}");
+	      Matcher matcher = pattern.matcher(s);
+	      System.out.println(matcher.matches());
+	    }
+
+	}
+
+}
